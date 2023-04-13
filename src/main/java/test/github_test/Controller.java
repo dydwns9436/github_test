f34900b5f51b0ec0dfad8f6f615c670696aaafc9
dydwns9436 <dydwns9436@gmail.com>
2023-04-13 17:06:35 +0900

    initial

---
+++ src/main/java/test/github_test/Controller.java
@@ -0,0 +1,13 @@
+package test.github_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping
+    public String helloWorld() {
+        return "To-do Application !";
+    }
+}
